b1464787c2b90a118efcbc7bfee30d0b5cb0e58a
luckboy心 <1062329969@qq.com>
2018-06-13 14:33:02 +0800

    提交

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'xcx_xnxna_com',
+    'database'        => 'xnxna',
     // 用户名
-    'username'        => 'xcx_xnxna_com',
+    'username'        => 'root',
     // 密码
-    'password'        => 'xcx_xnxna_com',
+    'password'        => 'root',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
