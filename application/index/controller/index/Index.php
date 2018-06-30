54f5265b67452d5088b3673b8d7825652938aafb
luckboy心 <1062329969@qq.com>
2018-06-30 14:38:07 +0800

    提交

--- application/index/controller/index/Index.php
+++ application/index/controller/index/Index.php
@@ -11,7 +11,12 @@ class Index extends Xcx
     }
 
     public function getlist(){
-        $list = Db::name('article')->where(['a_state'=>1])->order('a_id desc')->paginate(4)->toArray();
+        $list = Db::name('article')->where(['a_state'=>1])->order('a_id desc')->paginate(4);
+        foreach ($list as $k=>$v){
+//            var_dump($list[$k]);
+            $v['a_img'] = json_decode($v['a_img'],true)[0];
+            $list[$k] = $v;
+        }
         return json($list);
     }
 
